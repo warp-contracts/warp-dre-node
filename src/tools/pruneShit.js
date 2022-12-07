c3da886802d27de40e8c721b51978ef6a00cdba1
ppe <peter@redstone.finance>
2022-12-07 16:17:51 +0100

    chore: prune shit

---
+++ src/tools/pruneShit.js
@@ -0,0 +1,14 @@
+const {LmdbCache} = require("warp-contracts-lmdb");
+
+const {defaultCacheOptions} = require("warp-contracts");
+
+const lmdb = new LmdbCache({
+  ...defaultCacheOptions,
+  dbLocation: `./cache/warp/lmdb/state`
+});
+
+
+(async () => {
+  const stats = await lmdb.prune(3);
+  console.log(stats);
+})();
