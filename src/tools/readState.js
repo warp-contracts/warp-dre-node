6a1a853fa4097c6c3233bf46b81dda7db24f6d2e
ppe <peter@redstone.finance>
2023-02-08 15:51:07 +0100

    feat: kv storage support

--- src/tools/readState.js
+++ src/tools/readState.js
@@ -20,15 +20,20 @@ async function readState() {
       })
     );
 
-  const result = await warp
+  const result = await warp.contract("pChLLDZ3tuj4g4mqSS-QbXjqVRciV2wQdbnKQlK_t_A")
+    .getStorageValue("33F0QHcb22W7LwWR1iRC8Az1ntZG09XQ03YWuw2ABqA");
+
+  /*const result = await warp
     .contract('pbabEjmdaqOvF-yTkFhs5i2lbmmbC6s4NrUqM_8eAYE')
     .setEvaluationOptions({
       allowBigInt: true
     })
-    .readState();
+    .readState();*/
 
-  console.log(Object.keys(result.cachedValue.validity).length);
-  console.log(result.cachedValue.validity['TfUtCyTPAnnoX3a69IP_Eo9td9unTu2G_0V_VnVKNR8']);
+  console.log(result);
+
+  //console.log(Object.keys(result.cachedValue.validity).length);
+  //console.log(result.cachedValue.validity['TfUtCyTPAnnoX3a69IP_Eo9td9unTu2G_0V_VnVKNR8']);
 }
 
 readState().finally();
