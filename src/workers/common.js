59110e4b76df6e27fb79a7a2e103e548919ec9e8
Asia <ziola.jm@gmail.com>
2023-07-21 10:49:11 +0200

    feat: publish interact writes to redis (#48)

--- src/workers/common.js
+++ src/workers/common.js
@@ -1,14 +1,6 @@
 const { insertState, connect } = require('../db/nodeDb');
-const { publish: appSyncPublish, initPubSub: initAppSyncPublish } = require('warp-contracts-pubsub');
-const Redis = require('ioredis');
+const { publishToRedis, publishToAppSync } = require('./publish');
 const { config } = require('../config');
-
-initAppSyncPublish();
-
-const redisPublisher = new Redis({
-  ...config.gwPubSubConfig,
-  lazyConnect: false
-});
 
 module.exports = {
   storeAndPublish: async (logger, isTest, contractTxId, result) => {
@@ -17,41 +9,18 @@ module.exports = {
         logger.info('State stored in sqlite', contractTxId);
 
         if (!isTest) {
-          try {
-            redisPublisher.publish(
-              'states',
-              JSON.stringify({
-                contractTxId: dbResult.contract_tx_id,
-                sortKey: dbResult.sort_key,
-                state: dbResult.state,
-                node: dbResult.manifest.walletAddress,
-                signature: dbResult.signature,
-                manifest: dbResult.manifest,
-                stateHash: dbResult.state_hash
-              })
-            );
-            logger.debug('Published to Redis', contractTxId);
-          } catch (e) {
-            logger.error('Error while publishing to Redis');
-          }
+          publishToRedis(logger, contractTxId, {
+            contractTxId: dbResult.contract_tx_id,
+            sortKey: dbResult.sort_key,
+            state: dbResult.state,
+            node: dbResult.manifest.walletAddress,
+            signature: dbResult.signature,
+            manifest: dbResult.manifest,
+            stateHash: dbResult.state_hash
+          });
 
           if (config.appSync.publishState) {
-            logger.info('Publishing to appSync');
-            appSyncPublish(
-              `states/${contractTxId}`,
-              JSON.stringify({
-                sortKey: result.sortKey,
-                state: result.cachedValue.state,
-                signature: dbResult.signature
-              }),
-              config.appSync.key
-            )
-              .then(() => {
-                logger.debug(`Published to appSync ${contractTxId}`);
-              })
-              .catch((e) => {
-                logger.error('Error while publishing to AppSync', e);
-              });
+            publishToAppSync(logger, contractTxId, result, dbResult);
           }
         }
       })
