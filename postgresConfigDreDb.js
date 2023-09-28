edb5dd397c3a3bf893c1a84e6616ae7d2b3a0472
ppedziwiatr <ppedziwiatr@gmail.com>
2023-09-28 11:45:15 +0200

    pg ssl config

--- postgresConfigDreDb.js
+++ postgresConfigDreDb.js
@@ -3,14 +3,16 @@ module.exports = {
   max: 20,
   application_name: process.env.MY_NAME_IS,
   host: process.env.PG_HOST,
-  user: 'dre',
+  user: process.env.PG_USER_DRE,
   password: process.env.PG_USER_DRE_PASSWORD,
   database: process.env.PG_DATABASE.toLowerCase(),
   port: process.env.PG_PORT,
-  /*ssl: {
-    rejectUnauthorized: false,
-    ca: readFileSync('certs/db/ca.pem').toString(),
-    key: readFileSync('certs/warp/key.pem').toString(),
-    cert: readFileSync('certs/warp/cert.pem').toString()
-  }*/
+  ...(process.env.PG_SSL === "true" ? {
+    ssl: {
+      rejectUnauthorized: false,
+      ca: readFileSync('certs/db/ca.pem').toString(),
+      key: readFileSync('certs/db/dre/key.pem').toString(),
+      cert: readFileSync('certs/db/dre/cert.pem').toString()
+    }
+  } : ''),
 };
--- postgresConfigWarpDb.js
+++ postgresConfigWarpDb.js
@@ -4,14 +4,16 @@ module.exports = {
   maxEntriesPerContract: 10000000,
   application_name: process.env.MY_NAME_IS,
   host: process.env.PG_HOST,
-  user: 'warp',
+  user: process.env.PG_USER_WARP,
   password: process.env.PG_USER_WARP_PASSWORD,
   database: process.env.PG_DATABASE.toLowerCase(),
   port: process.env.PG_PORT,
- /* ssl: {
-    rejectUnauthorized: false,
-    ca: readFileSync('certs/db/ca.pem').toString(),
-    key: readFileSync('certs/warp/key.pem').toString(),
-    cert: readFileSync('certs/warp/cert.pem').toString()
-  }*/
+  ...(process.env.PG_SSL === "true" ? {
+    ssl: {
+      rejectUnauthorized: false,
+      ca: readFileSync('certs/db/ca.pem').toString(),
+      key: readFileSync('certs/db/warp/key.pem').toString(),
+      cert: readFileSync('certs/db/warp/cert.pem').toString()
+    }
+  } : ''),
 };
