8cfb788b1fd36d714506eb933c5a6ecb3bdf185f
Tadeuchi <tadeuszwl@gmail.com>
2023-11-30 10:31:56 +0100

    queues cleaning update

--- src/tools/cleanThemQueues.js
+++ src/tools/cleanThemQueues.js
@@ -1,28 +1,32 @@
 // nobody wants to wait in line...
-const { postEvalQueue, updateQueue } = require('../bullQueue');
+const { Queue } = require('bullmq');
+const { config } = require('../config');
+
+const GRACE_PERIOD = 60_000; // 1 minute
+const CLEAN_LIMIT = 50_000; // max number of jobs to clean
+const logger = require('../logger')('queuesClean');
 
 (async () => {
+  logger.info(`Queueueues clean started..`);
+  const postEvalQueue = configureQueue('postEval');
   const postEvalCount = await postEvalQueue.getJobCountByTypes();
-  console.log(`postEval queue count  ${postEvalCount}`);
-  if (postEvalCount > 5_000) {
-    console.log(`postEval queue - obliterate`);
-    await postEvalQueue.obliterate();
-  } else if (postEvalCount > 1_000) {
-    console.log(`postEval queue - drain`);
-    await postEvalQueue.drain();
+  logger.info(`postEval queue count  ${postEvalCount}`);
+  if (postEvalCount > 20_000) {
+    const prioritizedIds = await postEvalQueue.clean(GRACE_PERIOD, CLEAN_LIMIT, 'prioritized');
+    const waitIds = await postEvalQueue.clean(GRACE_PERIOD, CLEAN_LIMIT, 'wait');
+    logger.info(`Just removed prioritized jobs in count of ${prioritizedIds.length} and waiting jobs ${waitIds.length}`);
   }
-
-  // const updateCount = await updateQueue.getJobCountByTypes();
-  // console.log(`update queue count  ${updateCount}`);
-  // if (updateCount > 5_000) {
-  //   console.log(`update queue - obliterate`);
-  //   await updateQueue.obliterate();
-  // } else if (updateCount > 1_000) {
-  //   console.log(`update queue - drain`);
-  //   await updateQueue.drain();
-  // }
+  await postEvalQueue.close();
 })().then(() => {
-  console.log(`Finished..`);
-  console.log(`Yeah, you wish. Something definitely went wrong. You just don't know yet.`);
-  process.exit();
+  logger.info(`Finished..`);
 });
+
+function configureQueue(queueName) {
+  return new Queue(queueName, {
+    connection: config.bullMqConnection,
+    defaultJobOptions: {
+      removeOnComplete: true,
+      removeOnFail: true
+    }
+  });
+}
