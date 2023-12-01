857a13568f1892d0b399723c7bc415b18e1863dd
Tadeuchi <tadeuszwl@gmail.com>
2023-12-01 16:44:56 +0100

    increased restrictions

--- postgresConfigWarpDb.js
+++ postgresConfigWarpDb.js
@@ -1,7 +1,7 @@
 const { readFileSync } = require("fs");
 module.exports = {
-  minEntriesPerContract: 1000000,
-  maxEntriesPerContract: 10000000,
+  minEntriesPerContract: 100,
+  maxEntriesPerContract: 1000,
   application_name: process.env.MY_NAME_IS,
   host: process.env.PG_HOST,
   user: process.env.PG_USER_WARP,
--- src/warp.js
+++ src/warp.js
@@ -49,8 +49,8 @@ const warp = WarpFactory.forMainnet()
         ...warpDbConfig,
         schemaName: 'kv',
         tableName: contractTxId,
-        minEntriesPerKey: 1000,
-        maxEntriesPerKey: 10000,
+        minEntriesPerKey: 100,
+        maxEntriesPerKey: 10_000,
         application_name: 'kv'
       })
   )
